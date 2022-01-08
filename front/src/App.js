1ecba7f39a2433fc32a98836bfb09026e5b302c3
Priyanshu Upadhyay <puofficialid@gmail.com>
2022-01-08 22:56:29 +0530

    final touched to modal interface and minor front-end changes

--- front/src/App.js
+++ front/src/App.js
@@ -8,6 +8,19 @@ import './App.css'
 
 
 const App = () => {
+
+  const dateToJsFormat = (date) => {
+    date = date.getFullYear() + '-' + String(date.getMonth() + 1).padStart(2, '0') + '-' + String(date.getDate()).padStart(2, '0') + 'T' + String(date.getHours()).padStart(2, '0') + ':' + String(date.getMinutes()).padStart(2, '0');
+    return date
+  }
+
+  const capitalize = (data) => {
+    return (data.charAt(0).toUpperCase() + data.slice(1))
+  }
+
+  let today = new Date();
+  today = dateToJsFormat(today);
+
   const [toggle, setToggle] = useState(true);
   const flipToggle = () => { setToggle(!toggle) }
 
@@ -18,37 +31,37 @@ const App = () => {
   const [data, setData] = useState([]);
 
   const [modalInfo, setModalInfo] = useState([]);
+  
   const [show, setShow] = useState(false);
   const showModal = () => setShow(true);
   const closeModal = () => setShow(false);
 
-  const [checkbox, setCheck] = useState(false);
-  const uncheck = () => setCheck(false);
+
+  
   const [editShow, setEditShow] = useState(false);
-  const showEditModal = () => { setEditShow(true); };
-  const closeEditModal = () => { setEditShow(false); };
+  const showEditModal = () => setEditShow(true)
+  const closeEditModal = () => setEditShow(false)
+
+  const [editToggle, setEditToggle] = useState(true);
+  const flipEditToggle = () => setEditToggle(!editToggle)
 
   const [deleteShow, setDelete] = useState(false);
   const showDeleteModal = () => setDelete(true);
   const closeDeleteModal = () => setDelete(false);
 
-  const dateToJsFormat = (date) => { //2018-06-12T19:30
-    date = date.getFullYear() + '-' + String(date.getMonth() + 1).padStart(2, '0') + '-' + String(date.getDate()).padStart(2, '0') + 'T' + String(date.getHours()).padStart(2, '0') + ':' + String(date.getMinutes()).padStart(2, '0');
-    return date
-  }
 
-  const capitalize = (data) => {
-    return (data.charAt(0).toUpperCase() + data.slice(1))
-  }
 
   useEffect(() => {
     Axios.get('http://localhost:3001/getprojects').then((response) => {
-      console.log("Got the data")
+      console.log("Data reloaded!")
       setData(response.data)
     }).catch((err) => {
       console.log("Error: " + err.message)
     })
   }, [toggle]);
+
+
+
 
   const submit = () => {
     Axios.post("http://localhost:3001/addproject", {
@@ -70,42 +83,6 @@ const App = () => {
 
   }
 
-  const submitModal = (id) => {
-    Axios.post("http://localhost:3001/editproject", {
-      id: id,
-      name: modalInfo.name,
-      description: modalInfo.description,
-      deadline: modalInfo.deadline,
-      stack: modalInfo.techStack,
-      completed: checkbox
-    }).then((res) => {
-      console.log(res.data)
-      setModalInfo([])
-      flipToggle();
-      closeEditModal();
-      uncheck();
-    }).catch(err => {
-      uncheck();
-      console.log(`Error: ${err.message}`);
-    });
-
-  }
-
-  const deleteData = (id) => {
-    Axios.post('http://localhost:3001/deleteproject', {
-      id: id
-    })
-      .then(res => {
-        console.log(res.data);
-        setModalInfo([]);
-        flipToggle();
-        closeDeleteModal();
-      })
-      .catch(err => {
-        console.log(`Error: ${err.message}`);
-      })
-  }
-
 
   let pendingProjects = data.filter(project => project["completed"] === false)
   pendingProjects = pendingProjects.map(project => {
@@ -116,8 +93,9 @@ const App = () => {
           showModal();
         }}>{project.name}</li>
         <div>
-          <MdOutlineEditNote className='editIcon' onClick={async () => {
+          <MdOutlineEditNote className='editIcon' onClick={() => {
             setModalInfo(project);
+            flipEditToggle();
             showEditModal();
           }} />
           <MdDeleteOutline className='deleteIcon' onClick={() => {
@@ -154,19 +132,58 @@ const App = () => {
           Project Details:
         </Modal.Header>
         <Modal.Body className='modalBody'>
-          <div>Name:  </div><div className='modalIndividual'>{modalInfo.name}</div>
-          <div>Description: </div><div className='modalIndividual'>{modalInfo.description}</div>
-          <div>Deadline:  </div><div className='modalIndividual'>{date}</div>
-          <div>Tech Stack Used:</div><div className='modalIndividual'>{modalInfo.techStack}</div>
-          <div>Completed: </div><div className='modalIndividual'>{modalInfo.completed ? 'Yes' : 'No'}</div>
+          <div className='modalTitleDivContainer'><div className="modalTitleDivs">Name:</div><div className='modalIndividual'>{modalInfo.name}</div></div>
+          <div className='modalTitleDivContainer'><div className="modalTitleDivs">Description:</div><div className='modalIndividual'>{modalInfo.description}</div></div>
+          <div className='modalTitleDivContainer'><div className="modalTitleDivs">Deadline:</div><div className='modalIndividual'>{date}</div></div>
+          <div className='modalTitleDivContainer'><div className="modalTitleDivs">Tech Stack:</div><div className='modalIndividual'>{modalInfo.techStack}</div></div>
+          <div className='modalTitleDivContainer'><div className="modalTitleDivs">Completed:</div><div className='modalIndividual'>{modalInfo.completed ? 'Yes' : 'No'}</div></div>
         </Modal.Body>
       </Modal>
     )
   }
 
+
   const EditModal = () => {
     let today = new Date();
     today = dateToJsFormat(today);
+
+    const [editName, setEditName] = useState('');
+    const [editDescription, setEditDescription] = useState('');
+    const [editDeadline, setEditDeadline] = useState('');
+    const [editStack, setEditStack] = useState('');
+
+    const [checkbox, setCheck] = useState(false);
+    const uncheck = () => setCheck(false);
+
+    useEffect(() => {
+      setEditName(modalInfo.name)
+      setEditDescription(modalInfo.description)
+      setEditDeadline(dateToJsFormat(new Date(modalInfo.deadline)))
+      setEditStack(modalInfo.techStack)
+    }, [editToggle])
+
+    const submitModal = () => {
+      Axios.post("http://localhost:3001/editproject", {
+        id: modalInfo._id,
+        name: editName,
+        description: editDescription,
+        deadline: editDeadline,
+        stack: editStack,
+        completed: checkbox
+      }).then((res) => {
+        console.log(res.data)
+        setModalInfo([])
+        flipToggle();
+        closeEditModal();
+        uncheck();
+      }).catch(err => {
+        uncheck();
+        console.log(`Error: ${err.message}`);
+      });
+
+    }
+
+
 
     return (
       <Modal show={editShow} onHide={closeEditModal}>
@@ -176,22 +193,22 @@ const App = () => {
         <Modal.Body className='editModalBody'>
           <form className='formHolder editForm'>
             <label hidden>Project Name</label>
-            <input type="text" placeholder="Project Name" onChange={e => { modalInfo.name = e.target.value }} value={modalInfo.name} />
+            <input type="text" placeholder="Project Name" onChange={e => setEditName(e.target.value)} value={editName} />
 
             <label hidden>Project Description</label>
-            <textarea placeholder="Project Description" rows="3" columns="12" onChange={e => { modalInfo.description = e.target.value }} value={modalInfo.description} />
+            <textarea placeholder="Project Description" rows="3" columns="12" onChange={e => setEditDescription(e.target.value)} value={editDescription} />
 
             <label hidden>Project Deadline</label>
-            <input type="datetime-local" placeholder="Project Deadline" onChange={e => { modalInfo.deadline = e.target.value }} value={dateToJsFormat(new Date(modalInfo.deadline))} min={today} />
+            <input type="datetime-local" placeholder="Project Deadline" onChange={e => setEditDeadline(e.target.value)} value={editDeadline} min={today} />
 
-            <label hidden>Tech Stack used</label>
-            <input type="text" placeholder="Tech Stack" onChange={e => { modalInfo.techStack = e.target.value }} value={modalInfo.techStack} />
+            <label hidden>Tech Stack</label>
+            <input type="text" placeholder="Tech Stack" onChange={e => setEditStack(e.target.value)} value={editStack} />
 
             <div className="checkbox" >
               <label>Mark as complete</label>
               <input type="checkbox" defaultChecked={checkbox} onChange={(e) => { setCheck(e.target.checked); }} />
             </div>
-            <button type="button" onClick={() => submitModal(modalInfo._id)}>Done</button>
+            <button type="button" onClick={submitModal}>Done</button>
           </form>
         </Modal.Body>
       </Modal>
@@ -199,21 +216,34 @@ const App = () => {
   }
 
   const DeleteModal = () => {
+
+    const deleteData = () => {
+      Axios.post('http://localhost:3001/deleteproject', {
+        id: modalInfo._id
+      })
+        .then(res => {
+          console.log(res.data);
+          setModalInfo([]);
+          flipToggle();
+          closeDeleteModal();
+        })
+        .catch(err => {
+          console.log(`Error: ${err.message}`);
+        })
+    }
+    
     return (
       <Modal show={deleteShow} onHide={closeDeleteModal} className='delModalContainer'>
         <Modal.Header className='modalHead'>
           Delete {modalInfo.name}?
         </Modal.Header>
         <Modal.Body className='editModalBody delModal'>
-          <button onClick={() => deleteData(modalInfo._id)}>Yes</button>
+          <button onClick={deleteData}>Yes</button>
           <button onClick={closeDeleteModal}>No</button>
         </Modal.Body>
       </Modal>
     )
   }
-
-  let today = new Date();
-  today = dateToJsFormat(today);
 
   return (
     <div>
@@ -222,9 +252,8 @@ const App = () => {
           <img className="logo" src={logo} alt="logo" />
         </div>
         <div className="hac">
-          <span><a className='links' href="https://github.com/">Home</a></span>
-          <span><a className='links' href="https://github.com/">About</a></span>
-          <span><a className='links' href="https://github.com/">Contact</a></span>
+          <span><a className='links' href="https://github.com/PriyanshuUpadhyay">About</a></span>
+          <span><a className='links' href="mailto:puofficialid@gmail.com">Contact</a></span>
         </div>
       </nav>
 
@@ -275,7 +304,7 @@ const App = () => {
               <label hidden>Project Deadline</label>
               <input type="datetime-local" placeholder="Project Deadline" onChange={e => { setDeadline(e.target.value) }} value={deadline} min={today} />
 
-              <label hidden>Tech Stack used</label>
+              <label hidden>Tech Stack</label>
               <input type="text" placeholder="Tech Stack" onChange={e => { setStack(e.target.value) }} value={stack} />
 
               <button type="button" onClick={submit}>Done</button>
